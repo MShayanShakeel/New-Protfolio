0c363de1c48d1fa35f7d91104334cecfc6cd1cc6
MShayanShakeel <mshayanshakeel786@gmail.com>
2024-07-05 18:42:20 +0500

    updatte

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,20 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/img/SSimg3.PNG";
+import projImg2 from "../assets/img/SSimg1.PNG";
+import projImg3 from "../assets/img/SSimg2.PNG";
+import projImg4 from "../assets/img/Kingcoin.PNG";
+import projImg5 from "../assets/img/SSimg4.PNG";
+import projImg6 from "../assets/img/SSimg5.PNG";
+import projImg7 from "../assets/img/SSimg6.PNG";
+import projImg8 from "../assets/img/SSimg7.PNG";
+import projImg9 from "../assets/img/SSimg8.PNG";
+import projImg10 from "../assets/img/SSimg9.PNG";
+import projImg11 from "../assets/img/SSimg11.PNG";
+import projImg12 from "../assets/img/SSimg12.PNG";
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
+
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import { RiGraduationCapFill } from "react-icons/ri";
@@ -16,42 +27,79 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "WatsPI",
-      description: "WatsPI: A MERN stack project showcasing seamless integration of MongoDB, Express.js, React.js, and Node.js for robust, efficient web solutions.",
-      imgUrl: projImg1,
-      link: "https://watspi-user-plum.vercel.app/dashboard2",
-    },
-    {
-      title: "WatsPI-White-Theme",
-      description: "WatsPI: A MERN stack project featuring a white theme, demonstrating seamless integration of MongoDB, Express.js, React.js, and Node.js for robust web solutions.",
-      imgUrl: projImg2,
-      link: "https://production-witspi.vercel.app/dashboard2",
-
-    },
-    {
-      title: "E-Commerce Project",
-      description: "E-Commerce website prototype has an impressive user interface (UI) and dynamic functionalities, but the backend isn't deployed. Despite the lack of a visible UI, the backend still includes important features like user authentication, tokenization, and more.",
-      imgUrl: projImg3,
-      link: "https://mern-front-end-only.vercel.app/",
-    },
-    {
       title: "Insta Pay",
       description: "Insta Pay prototype boasts an impressive user interface (UI), featuring Tailwind CSS for styling and possibly including chart graphs. While the UI looks polished and user-friendly, it seems this is only a frontend prototype without backend functionality",
       imgUrl: projImg1,
       link: "https://instapayclone-2.vercel.app/",
     },
     {
-      title: "BAZAR.COM",
-      description: "AZAR.COM: A university project showcasing my expertise in React.js, Redux, and Firebase. This eCommerce website prototype embodies seamless user interaction and dynamic functionalities.",
+      title: "Investment.trade.com",
+      description: "A mobile-responsive frontend UI built with React.js and Bootstrap for an investment platform, showcasing modern design and user-friendly navigation.",
       imgUrl: projImg2,
-      link: "https://dulcet-klepon-261381.netlify.app/",
+      link: "https://fb-task-web.vercel.app/",
+    },
+    {
+      title: "LinkedIn Clone",
+      description: " A comprehensive MERN stack project utilizing Redux, Context API, Thunk, tokenization authentication, and various additional features to replicate the functionality of LinkedIn.backend not working because not deploy",
+      imgUrl: projImg12,
+      link: "https://linkdin-clone-frontend.vercel.app/",
+    },
+    {
+      title: "Tourist.com",
+      description: "A mobile-responsive application developed using React.js, Bootstrap, Firebase, and forms, showcasing modern UI, real-time data, and user-friendly interactions.",
+      imgUrl: projImg3,
+      link: "https://job-finder-web-lac.vercel.app/",
     },
     {
       title: "Trading App",
       description: "I designed a trading app UI prototype, focusing solely on user interface practices to ensure an intuitive and visually appealing experience",
-      imgUrl: projImg3,
+      imgUrl: projImg4,
       link: "https://test-project-omega-seven.vercel.app/",
     },
+    {
+
+      title: "WatsPI-White-Theme",
+      description: "WatsPI: A MERN stack project featuring a white theme, demonstrating seamless integration of MongoDB, Express.js, React.js, and Node.js for robust web solutions.",
+      imgUrl: projImg5,
+      link: "https://production-witspi.vercel.app/dashboard2",
+    },
+    {
+      title: "WatsPI",
+      description: "WatsPI: A MERN stack project showcasing seamless integration of MongoDB, Express.js, React.js, and Node.js for robust, efficient web solutions.",
+      imgUrl: projImg6,
+      link: "https://watspi-user-plum.vercel.app/dashboard2",
+    },
+    {
+      title: "E-Commerce Project",
+      description: "E-Commerce website prototype has an impressive user interface (UI) , but the backend isn't deployed. Despite the lack of a visible UI, the backend includes user authentication, tokenization",
+      imgUrl: projImg7,
+      link: "https://mern-front-end-only.vercel.app/",
+    },
+    {
+      title: "RMI surfaces",
+      description: "A mobile-responsive website developed using plain HTML and CSS, showcasing a clean design and smooth user experience.",
+      imgUrl: projImg8,
+      link: "https://66854ad6756ef1078c21a4ea--incredible-cannoli-77c5df.netlify.app/",
+    },
+    {
+      title: "BAZAR.COM",
+      description: "AZAR.COM: A university project showcasing my expertise in React.js, Redux, and Firebase. This eCommerce website prototype embodies seamless user interaction and dynamic functionalities.",
+      imgUrl: projImg9,
+      link: "https://dulcet-klepon-261381.netlify.app/",
+    },
+    {
+      title: "Code Snippet",
+      description: "A project developed using Angular 17, Firebase, and Angular authentication, offering secure and efficient code management and sharing features.",
+      imgUrl: projImg10,
+      link: "https://bin-application-6c92e.web.app/singup",
+    },
+    {
+      title: "Authentication Form",
+      description: "A project developed using React.js and React Auth, providing a secure and user-friendly authentication system for web applications.",
+      imgUrl: projImg11,
+      link: "https://form-validation-ten-iota.vercel.app/",
+    },
+
   ];
 
   return (
